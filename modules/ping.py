3fd9a4280ba0c9c4870c86c641d786c4a017e874
NotKiwy <c3zYyyyx.offical@gmail.com>
2024-11-21 22:30:46 +0300

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -1,18 +1,16 @@
-# ping
-# 1.0.0
-# SQUID-SQUID LLC
-# Simple ping module. Created by SQUID-SQUID LLC.
+#ping
+#1.0.0
+#SQUID-SQUID LLC
+#Simple ping module. Created by SQUID-SQUID LLC.
 from pyrogram import Client, filters
 import time
 import asyncio
-
 
 async def loading(message):
     frames = ["◾◽◽◽", "◽◾◽◽", "◽◽◾◽", "◽◽◽◾", "◽◽◾◽", "◽◾◽◽"]
     for frame in frames:
         await message.edit(f"◾ `Loading... {frame}`")
         await asyncio.sleep(0.2)
-
 
 @Client.on_message(filters.command("ping", prefixes="#") & filters.me)
 async def ping(client, message):
