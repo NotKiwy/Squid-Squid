e08e332df103efab8206e217a821ad506401b327
NotKiwy <c3zYyyyx.offical@gmail.com>
2024-11-23 22:34:42 +0300

    Update example.py

--- modules/example.py
+++ modules/example.py
@@ -1,9 +1,8 @@
 #example
 #1.0.0
-#SQUID-SQUID LLC
-#Example of module.
+
 from pyrogram import filters
 
 @app.on_message(filters.command("example", prefixes="#") & filters.me)
-async def ping_cmd(_, message):
-    await message.reply("It works!")
+async def example_cmd(_, message):
+    await message.edit_text("**Hey there!**")
