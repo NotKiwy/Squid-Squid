28c20f4acf7e55b84f71d7c72199549c3c828426
NotKiwy <c3zYyyyx.offical@gmail.com>
2024-11-23 22:36:44 +0300

    Update example.py

--- modules/example.py
+++ modules/example.py
@@ -1,8 +1,8 @@
 #example
 #1.0.0
-
 from pyrogram import filters
 
-@app.on_message(filters.command("example", prefixes="#") & filters.me)
+@app.on_message(filters.me)
 async def example_cmd(_, message):
-    await message.edit_text("**Hey there!**")
+    if message.text == "#example":
+        await message.edit("It works!")
